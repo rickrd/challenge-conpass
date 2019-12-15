86441693137eecb3b71e106763ac496d378f0e8c
rickrd <desouzaricardoc@gmail.com>
2019-12-14 23:42:31 -0300

    added removeHotspot action

--- src/components/redux/actions.js
+++ src/components/redux/actions.js
@@ -1,10 +1,15 @@
 export const ADD_HOTSPOT = 'ADD_HOTSPOT'
+export const REMOVE_HOTSPOT = 'REMOVE_HOTSPOT'
 export const EDIT_HOTSPOT_TITLE = 'EDIT_HOTSPOT_TITLE'
 export const EDIT_HOTSPOT_DESCRIPTION = 'EDIT_HOTSPOT_DESCRIPTION'
 export const EDITABLE_HOTSPOT = 'EDITABLE_HOTSPOT'
 
 export function addHotspot(title, description, x, y, editable) {
-  return { type: ADD_HOTSPOT, title, description, x, y, editable}
+  return { type: ADD_HOTSPOT, title, description, x, y, editable }
+}
+
+export function removeHotspot(index) {
+  return { type: REMOVE_HOTSPOT, index }
 }
 
 export function editHotspotTitle(title, index) {
